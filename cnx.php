d0816471ec860e5e02978b4c97312a68065e588b
Systemsadms <systemsadms@hotmail.com>
2020-08-25 04:08:32 -0430

    se agrego el archivo cnx

---
+++ cnx.php
@@ -0,0 +1,19 @@
+<?php
+/*
+
+$servidor = "localhost";              
+$usuario = "avelunco_admin";         
+$clave = "hdwtnkue456";              
+$db = "avelunco_avelun_bd";        
+$conexion=mysqli_connect("$servidor","$usuario","$clave","$db");
+*/
+
+
+$servidor = "localhost";              
+$usuario = "root";         
+$clave = "";              
+$db = "avelun_bd";        
+$conexion=mysqli_connect("$servidor","$usuario","$clave","$db");
+
+
+?>
